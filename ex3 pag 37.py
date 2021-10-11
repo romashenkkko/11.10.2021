e45eebfeab9abc1b199f67f0ffcf2e6f6fe21d04
romashenkkko <70690141+romashenkkko@users.noreply.github.com>
2021-10-11 09:54:38 +0300

    Add files via upload

---
+++ ex3 pag 37.py
@@ -0,0 +1,3 @@
+nume = input("Care este numele Dvs? " )
+prof = input("Ce profesie doriti sa aveti? " )
+print("Numele Dvs este {0} si profesia pe care doriti sa o aveti este {1}". format (nume, prof))
